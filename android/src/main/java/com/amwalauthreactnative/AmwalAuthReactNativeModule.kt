8e9e88c1c609965baa44b5056c057764a3a6ccb2
ahmed3elshaer <ahmed3elshaer@gmail.com>
2023-01-12 12:08:45 +0200

    modify activity register code

--- android/src/main/java/com/amwalauthreactnative/AmwalAuthReactNativeModule.kt
+++ android/src/main/java/com/amwalauthreactnative/AmwalAuthReactNativeModule.kt
@@ -2,35 +2,67 @@ package com.amwalauthreactnative
 
 import android.app.Activity
 import android.util.Base64
-import com.facebook.react.bridge.Arguments
-import com.facebook.react.bridge.ReactApplicationContext
-import com.facebook.react.bridge.ReactContextBaseJavaModule
-import com.facebook.react.bridge.ReactMethod
-import com.facebook.react.bridge.Promise
-import com.facebook.react.bridge.ReadableMap
-import com.facebook.react.bridge.WritableMap
-import com.google.android.gms.fido.Fido
-import com.google.android.gms.fido.fido2.Fido2ApiClient
-import com.google.android.gms.fido.fido2.api.common.*
-import androidx.appcompat.app.AppCompatActivity
 import androidx.activity.result.ActivityResult
 import androidx.activity.result.IntentSenderRequest
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import com.facebook.react.bridge.Arguments
+import com.facebook.react.bridge.Promise
+import com.facebook.react.bridge.ReactApplicationContext
+import com.facebook.react.bridge.ReactContextBaseJavaModule
+import com.facebook.react.bridge.ReactMethod
+import com.facebook.react.bridge.ReadableMap
+import com.google.android.gms.fido.Fido
+import com.google.android.gms.fido.fido2.Fido2ApiClient
+import com.google.android.gms.fido.fido2.api.common.AuthenticatorErrorResponse
+import com.google.android.gms.fido.fido2.api.common.PublicKeyCredential
+import com.google.android.gms.fido.fido2.api.common.PublicKeyCredentialCreationOptions
+import com.google.android.gms.fido.fido2.api.common.PublicKeyCredentialParameters
+import com.google.android.gms.fido.fido2.api.common.PublicKeyCredentialRpEntity
+import com.google.android.gms.fido.fido2.api.common.PublicKeyCredentialUserEntity
 
 private const val BASE64_FLAG = Base64.NO_PADDING or Base64.NO_WRAP or Base64.URL_SAFE
-
 fun ByteArray.toBase64(): String {
-    return Base64.encodeToString(this, BASE64_FLAG)
+  return Base64.encodeToString(this, BASE64_FLAG)
 }
 
 fun String.decodeBase64(): ByteArray {
-    return Base64.decode(this, BASE64_FLAG)
+  return Base64.decode(this, BASE64_FLAG)
 }
 
 class AmwalAuthReactNativeModule(val reactContext: ReactApplicationContext) :
   ReactContextBaseJavaModule(reactContext) {
-
+  private var promise: Promise? = null
   private var fido2ApiClient: Fido2ApiClient? = Fido.getFido2ApiClient(reactContext);
+  private val currentActivity = reactContext.getCurrentActivity() as AppCompatActivity;
+  private val createCredentialIntentLauncher =
+    currentActivity.registerForActivityResult(ActivityResultContracts.StartIntentSenderForResult()) { activityResult: ActivityResult ->
+      if (activityResult.resultCode != Activity.RESULT_OK) {
+        promise?.reject(null, "User Canceled")
+      } else {
+        val bytes = activityResult.data?.getByteArrayExtra(Fido.FIDO2_KEY_CREDENTIAL_EXTRA);
+        if (bytes == null) {
+          promise?.reject(null, "Credential Registration Error")
+        } else {
+          val credential = PublicKeyCredential.deserializeFromBytes(bytes)
+          val response = credential.getResponse()
+          if (response is AuthenticatorErrorResponse) {
+            promise?.reject(null, response.errorMessage)
+          } else {
+            val map = Arguments.createMap()
+            val rawId = credential.rawId.toBase64()
+            map.putString("id", rawId)
+            map.putString("rawId", rawId)
+            map.putString("type", "public-key")
+            val responseMap = Arguments.createMap()
+            //responseMap.putString("attestationObject", response.attestationObject.toBase64())
+            responseMap.putString("clientDataJSON", response.clientDataJSON.toBase64())
+            map.putMap("response", responseMap)
+            promise?.resolve(map)
+          }
+        }
+      }
+    }
 
   override fun getName(): String {
     return NAME
@@ -44,24 +76,33 @@ class AmwalAuthReactNativeModule(val reactContext: ReactApplicationContext) :
     val builder = PublicKeyCredentialCreationOptions.Builder()
     builder.setChallenge(creationOptionsJSON.getString("challenge")!!.decodeBase64())
     val user = creationOptionsJSON.getMap("user")
-    builder.setUser(PublicKeyCredentialUserEntity(
-      user?.getString("id")?.decodeBase64()!!,
-      user?.getString("name")!!, 
-      null.toString(), // icon
-      user?.getString("displayName")?: ""
-    ))
+    builder.setUser(
+      PublicKeyCredentialUserEntity(
+        user?.getString("id")?.decodeBase64()!!,
+        user?.getString("name")!!,
+        null.toString(), // icon
+        user?.getString("displayName") ?: ""
+      )
+    )
     val rp = creationOptionsJSON.getMap("rp")
-    builder.setRp(PublicKeyCredentialRpEntity(
-      rp?.getString("id")!!,
-      rp?.getString("name")!!,
-      /* icon */ null
-    ))
+    builder.setRp(
+      PublicKeyCredentialRpEntity(
+        rp?.getString("id")!!,
+        rp?.getString("name")!!,
+        /* icon */ null
+      )
+    )
     val pubKeyCredParams = creationOptionsJSON.getArray("pubKeyCredParams")?.toArrayList()
-    if (pubKeyCredParams != null){
-      val parameters = mutableListOf<PublicKeyCredentialParameters>() 
+    if (pubKeyCredParams != null) {
+      val parameters = mutableListOf<PublicKeyCredentialParameters>()
       for (param in pubKeyCredParams) {
         if (param is ReadableMap) {
-          parameters.add( PublicKeyCredentialParameters( param.getString("type")!!, param.getInt("alg")))
+          parameters.add(
+            PublicKeyCredentialParameters(
+              param.getString("type")!!,
+              param.getInt("alg")
+            )
+          )
         }
       }
       builder.setParameters(parameters)
@@ -71,36 +112,9 @@ class AmwalAuthReactNativeModule(val reactContext: ReactApplicationContext) :
 
   @ReactMethod
   fun startRegistration(creationOptionsJSON: ReadableMap, promise: Promise) {
+    this.promise = promise
     fido2ApiClient?.let { client ->
       try {
-        val currentActivity = reactContext.getCurrentActivity() as AppCompatActivity;
-        val createCredentialIntentLauncher = currentActivity.registerForActivityResult(ActivityResultContracts.StartIntentSenderForResult()) { activityResult: ActivityResult ->
-          if (activityResult.resultCode != Activity.RESULT_OK){
-            promise.reject(null, "User Canceled")
-          } else {
-            val bytes = activityResult.data?.getByteArrayExtra(Fido.FIDO2_KEY_CREDENTIAL_EXTRA);
-            if (bytes == null){
-              promise.reject(null, "Credential Registration Error")
-            } else {
-              val credential = PublicKeyCredential.deserializeFromBytes(bytes)
-              val response = credential.getResponse()
-              if (response is AuthenticatorErrorResponse) {
-                promise.reject(null, response.errorMessage)
-              } else {
-                val map = Arguments.createMap()
-                val rawId = credential.rawId.toBase64()
-                map.putString("id", rawId)
-                map.putString("rawId", rawId)
-                map.putString("type", "public-key")
-                val responseMap = Arguments.createMap()
-                //responseMap.putString("attestationObject", response.attestationObject.toBase64())
-                responseMap.putString("clientDataJSON", response.clientDataJSON.toBase64())
-                map.putMap("response", responseMap)
-                promise.resolve(map)
-              }
-            }
-          }
-        }
         val requestOptions = parseCreationOptions(creationOptionsJSON)
         val task = client.getRegisterPendingIntent(requestOptions)
         task.addOnSuccessListener {
@@ -110,7 +124,7 @@ class AmwalAuthReactNativeModule(val reactContext: ReactApplicationContext) :
             )
           }
         }
-      }catch (e: Exception) {
+      } catch (e: Exception) {
         promise.reject(null, e)
       }
     }
